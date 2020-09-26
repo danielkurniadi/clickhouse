37637b8f63618a9fb3ffcc743fd924b0ec53b41d
iqdf <daniel.thekurniadi@gmail.com>
2020-09-26 19:43:22 +0800

    Implement migrator methods with clickhouse syntax - CreateTable, HasTable - AlterColumn, RenameColumn, HasColumn - CurrentDatabase - CreateIndex, RenameIndex, DropIndex, BuildIndexOpt

--- migrator.go
+++ migrator.go
@@ -1,7 +1,6 @@
 package clickhouse
 
 import (
-	"database/sql"
 	"errors"
 	"fmt"
 	"strings"
@@ -12,16 +11,23 @@ import (
 	"gorm.io/gorm/schema"
 )
 
+// Default values for any SQL options
+const (
+	DefaultIndexType   = "minmax" // index stores extremes of the expression
+	DefaultGranularity = 1        // 1 granule = 8192 rows
+)
+
+// Errors enumeration
+var (
+	ErrRenameColumnUnsupported = errors.New("renaming column is not supported in your clickhouse version < 20.4.")
+	ErrRenameIndexUnsupported  = errors.New("renaming index is not supported")
+	ErrCreateIndexFailed       = errors.New("failed to create index with name")
+)
+
 type Migrator struct {
 	migrator.Migrator
 	Dialector
 }
-
-// Errors enumeration
-var (
-	ErrRenameIndexUnsupported = errors.New("renaming index is not supported in clickhouse")
-	ErrCreateIndexFailed      = errors.New("failed to create index with name")
-)
 
 // Database
 
@@ -45,49 +51,75 @@ func (m Migrator) CreateTable(models ...interface{}) error {
 		tx := m.DB.Session(new(gorm.Session))
 		if err := m.RunWithValue(model, func(stmt *gorm.Statement) (err error) {
 			var (
-				createTableSQL = "CREATE TABLE ? (%s %s %s) ENGINE=%s"
+				createTableSQL = "CREATE TABLE ? (%s %s, %s) ENGINE=%s"
 				args           = []interface{}{clause.Table{Name: stmt.Table}}
 			)
 
+			// Step 1. Build column datatype SQL string
 			columnSlice := make([]string, 0, len(stmt.Schema.DBNames))
 			for _, dbName := range stmt.Schema.DBNames {
 				field := stmt.Schema.FieldsByDBName[dbName]
 				columnSlice = append(columnSlice, "? ?")
-				args = append(args, clause.Column{Name: dbName}, m.DB.Migrator().FullDataTypeOf(field))
+				args = append(args,
+					clause.Column{Name: dbName},
+					m.DB.Migrator().FullDataTypeOf(field),
+				)
 			}
 			columnStr := strings.Join(columnSlice, ",")
 
+			// Step 2. Build constraint check SQL string if any constraint
 			constrSlice := make([]string, 0, len(columnSlice))
 			for _, check := range stmt.Schema.ParseCheckConstraints() {
 				constrSlice = append(constrSlice, "CONSTRAINT ? CHECK ?")
-				args = append(args, clause.Column{Name: check.Name}, clause.Expr{SQL: check.Constraint})
+				args = append(args,
+					clause.Column{Name: check.Name},
+					clause.Expr{SQL: check.Constraint},
+				)
 			}
 			constrStr := strings.Join(constrSlice, ",")
 
+			// Step 3. Build index SQL string
+			// NOTE: index class [UNIQUE | FULLTEXT | SPATIAL] is NOT supported!
 			indexSlice := make([]string, 0, 10)
 			for _, index := range stmt.Schema.ParseIndexes() {
 				if m.CreateIndexAfterCreateTable {
 					defer func(model interface{}, indexName string) {
+						// TODO (iqdf): what if there are multiple errors
+						// when creating indices after create table?
 						err = tx.Migrator().CreateIndex(model, indexName)
 					}(model, index.Name)
 					continue
 				}
-				str := "INDEX ? ?"
-				if index.Class != "" {
-					str = index.Class + " " + str
-				}
-				indexSlice = append(indexSlice, str)
 
+				// TODO(iqdf): support primary key by put it as pass the fieldname
+				// as MergeTree(...) parameters. But somehow it complained.
+				// Note that primary key doesn't ensure uniqueness
+
+				// Get indexing type `gorm:"index,type:minmax"`
+				// Choice: minmax | set(n) | ngrambf_v1(n, size, hash, seed) | bloomfilter()
+				indexType := DefaultIndexType
+				if index.Type != "" {
+					indexType = index.Type
+				}
+
+				// Get expression for index options
+				// Syntax: (`colname1`, ...)
 				buildIndexOptions := tx.Migrator().(migrator.BuildIndexOptionsInterface)
 				indexOptions := buildIndexOptions.BuildIndexOptions(index.Fields, stmt)
+
+				// Stringify index builder
+				// TODO (iqdf): support granularity
+				str := fmt.Sprintf("INDEX ? ? TYPE %s GRANULARITY %d", indexType, DefaultGranularity)
+				indexSlice = append(indexSlice, str)
 				args = append(args, clause.Expr{SQL: index.Name}, indexOptions)
 			}
-			indexStr := strings.Join(indexSlice, ",")
+			indexStr := strings.Join(indexSlice, ", ")
 
-			// Finally assemble CREATE TABLE ... SQL string consisting of
-			// column names string, constraint/check string, indexes name string
-			// followed by engine options
-			createTableSQL = fmt.Sprintf(createTableSQL, columnStr, constrStr, indexStr, "MergeTree()")
+			// Step 4. Finally assemble CREATE TABLE ... SQL string
+			engineOpts := "MergeTree() ORDER BY tuple()"
+			createTableSQL = fmt.Sprintf(createTableSQL, columnStr, constrStr, indexStr, engineOpts)
+			err = tx.Exec(createTableSQL, args...).Error
+
 			return
 		}); err != nil {
 			return err
@@ -96,27 +128,20 @@ func (m Migrator) CreateTable(models ...interface{}) error {
 	return nil
 }
 
-func (m Migrator) DropTable(dsts ...interface{}) error {
-	return nil
-}
-
-func (m Migrator) HasTable(dst interface{}) bool {
-	return false
-}
-
-func (m Migrator) RenameTable(oldName, newName interface{}) error {
-	return nil
+func (m Migrator) HasTable(value interface{}) bool {
+	var count int64
+	m.RunWithValue(value, func(stmt *gorm.Statement) error {
+		currentDatabase := m.DB.Migrator().CurrentDatabase()
+		return m.DB.Raw(
+			"SELECT count(*) FROM system.tables WHERE database = ? AND name = ? AND is_temporary = ?",
+			currentDatabase,
+			stmt.Table,
+			uint8(0)).Row().Scan(&count)
+	})
+	return count > 0
 }
 
 // Columns
-
-func (m Migrator) AddColumn(dst interface{}, field string) error {
-	return nil
-}
-
-func (m Migrator) DropColumn(dst interface{}, field string) error {
-	return nil
-}
 
 func (m Migrator) AlterColumn(value interface{}, field string) error {
 	return m.RunWithValue(value, func(stmt *gorm.Statement) error {
@@ -128,24 +153,15 @@ func (m Migrator) AlterColumn(value interface{}, field string) error {
 				m.FullDataTypeOf(field),
 			).Error
 		}
-		return fmt.Errorf("failed to look up field with name: %s", field)
+		return fmt.Errorf("altercolumn() failed to look up column with name: %s", field)
 	})
 }
 
-func (m Migrator) MigrateColumn(dst interface{}, field *schema.Field, columnType *sql.ColumnType) error {
-	return nil
-}
-
-func (m Migrator) HasColumn(dst interface{}, field string) bool {
-	return false
-}
-
-// NOTE: Only supported after ClickHouse 20.4 and above. Before v20.4,
-// renaming column implementation is not trivial, performing rename column
-// without reworking of ALTERs will cause race conditions in replicated tables
+// NOTE: Only supported after ClickHouse 20.4 and above.
+// See: https://github.com/ClickHouse/ClickHouse/issues/146
 func (m Migrator) RenameColumn(value interface{}, oldName, newName string) error {
 	return m.RunWithValue(value, func(stmt *gorm.Statement) error {
-		if m.Dialector.DontSupportRenameColumn {
+		if !m.Dialector.DontSupportRenameColumn {
 			var field *schema.Field
 			if f := stmt.Schema.LookUpField(oldName); f != nil {
 				oldName = f.DBName
@@ -163,33 +179,43 @@ func (m Migrator) RenameColumn(value interface{}, oldName, newName string) error
 					clause.Column{Name: newName},
 				).Error
 			}
-			return fmt.Errorf("failed to look up field with name: %s", newName)
+			return fmt.Errorf("renamecolumn() failed to look up column with name: %s", oldName)
 		}
-		return fmt.Errorf(
-			"failed to rename column. renaming column is disabled or isn't supported")
+		return ErrRenameIndexUnsupported
 	})
 }
 
-func (m Migrator) ColumnTypes(dst interface{}) ([]*sql.ColumnType, error) {
-	return []*sql.ColumnType{}, nil
+func (m Migrator) HasColumn(value interface{}, field string) bool {
+	var count int64
+	m.RunWithValue(value, func(stmt *gorm.Statement) error {
+		currentDatabase := m.DB.Migrator().CurrentDatabase()
+		name := field
+		if field := stmt.Schema.LookUpField(field); field != nil {
+			name = field.DBName
+		}
+
+		return m.DB.Raw(
+			"SELECT count(*) FROM system.columns WHERE database = ? AND table = ? AND name = ?",
+			currentDatabase, stmt.Table, name,
+		).Row().Scan(&count)
+	})
+
+	return count > 0
 }
 
 // Indexes
 
-// Table Index: https://clickhouse.tech/docs/en/sql-reference/statements/alter/index/
 func (m Migrator) BuildIndexOptions(opts []schema.IndexOption, stmt *gorm.Statement) (results []interface{}) {
-	// TODO (iqdf): // type value name
-	for _, opt := range opts {
-		str := stmt.Quote(opt.DBName)
-		if opt.Expression != "" {
-			str = opt.Expression
+	for _, indexOpt := range opts {
+		str := stmt.Quote(indexOpt.DBName)
+		if indexOpt.Expression != "" {
+			str = indexOpt.Expression
 		}
 		results = append(results, clause.Expr{SQL: str})
 	}
 	return
 }
 
-// Table Index: https://clickhouse.tech/docs/en/sql-reference/statements/alter/index/
 func (m Migrator) CreateIndex(value interface{}, name string) error {
 	return m.RunWithValue(value, func(stmt *gorm.Statement) error {
 		if idx := stmt.Schema.LookIndex(name); idx != nil {
@@ -200,10 +226,17 @@ func (m Migrator) CreateIndex(value interface{}, name string) error {
 				opts,
 			}
 
+			// Get indexing type `gorm:"index,type:minmax"`
+			// Choice: minmax | set(n) | ngrambf_v1(n, size, hash, seed) | bloomfilter()
+			indexType := DefaultIndexType
+			if idx.Type != "" {
+				indexType = idx.Type
+			}
+
 			// NOTE: concept of UNIQUE | FULLTEXT | SPATIAL index
-			// is not supported in clickhouse
-			createIndexSQL := "ALTER TABLE ? ADD INDEX ? ? TYPE %s GRANULARITY %d" // TODO (iqdf) how to inject Granularity
-			createIndexSQL = fmt.Sprintf(createIndexSQL, idx.Type, 0)              // Granularity: 0
+			// is NOT supported in clickhouse
+			createIndexSQL := "ALTER TABLE ? ADD INDEX ? ? TYPE %s GRANULARITY %d"      // TODO(iqdf): how to inject Granularity
+			createIndexSQL = fmt.Sprintf(createIndexSQL, indexType, DefaultGranularity) // Granularity: 1 (default)
 			return m.DB.Exec(createIndexSQL, values...).Error
 		}
 		return ErrCreateIndexFailed
@@ -211,6 +244,9 @@ func (m Migrator) CreateIndex(value interface{}, name string) error {
 }
 
 func (m Migrator) RenameIndex(value interface{}, oldName, newName string) error {
+	// TODO(iqdf): drop index and add the index again with different name
+	// DROP INDEX ?
+	// ADD INDEX ? TYPE ? GRANULARITY ?
 	return ErrRenameIndexUnsupported
 }
 
@@ -219,6 +255,9 @@ func (m Migrator) DropIndex(value interface{}, name string) error {
 		if idx := stmt.Schema.LookIndex(name); idx != nil {
 			name = idx.Name
 		}
-		return m.DB.Exec("DROP INDEX ?", clause.Column{Name: name}).Error
+		dropIndexSQL := "ALTER TABLE ? DROP INDEX ?"
+		return m.DB.Exec(dropIndexSQL,
+			clause.Table{Name: stmt.Table},
+			clause.Column{Name: name}).Error
 	})
 }
