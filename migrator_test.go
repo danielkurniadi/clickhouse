ab8f3e6caf09fef045f591e8b98b6e2d101a259d
Jinzhu <wosmvp@gmail.com>
2020-12-03 17:49:27 +0800

    Fix tests order

--- migrator_test.go
+++ migrator_test.go
@@ -1,8 +1,6 @@
 package clickhouse_test
 
 import (
-	"math/rand"
-	"testing"
 	"time"
 )
 
@@ -14,26 +12,4 @@ type User struct {
 	Age       int64
 	CreatedAt time.Time
 	UpdatedAt time.Time
-}
-
-func TestMigrate(t *testing.T) {
-	allModels := []interface{}{&User{}}
-	rand.Seed(time.Now().UnixNano())
-	rand.Shuffle(len(allModels), func(i, j int) { allModels[i], allModels[j] = allModels[j], allModels[i] })
-
-	DB.Migrator().DropTable("user_speaks", "user_friends")
-
-	if err := DB.Migrator().DropTable(allModels...); err != nil {
-		t.Fatalf("Failed to drop table, got error %v", err)
-	}
-
-	if err := DB.AutoMigrate(allModels...); err != nil {
-		t.Fatalf("Failed to auto migrate, but got error %v", err)
-	}
-
-	for _, m := range allModels {
-		if !DB.Migrator().HasTable(m) {
-			t.Fatalf("Failed to create table for %#v", m)
-		}
-	}
 }
--- tests_test.go
+++ tests_test.go
@@ -2,7 +2,9 @@ package clickhouse_test
 
 import (
 	"log"
+	"math/rand"
 	"os"
+	"time"
 
 	"gorm.io/driver/clickhouse"
 	"gorm.io/gorm"
@@ -21,7 +23,32 @@ func init() {
 		os.Exit(1)
 	}
 
+	RunMigrations()
+
 	if os.Getenv("DEBUG") == "true" {
 		DB = DB.Debug()
+	}
+}
+
+func RunMigrations() {
+	allModels := []interface{}{&User{}}
+	rand.Seed(time.Now().UnixNano())
+	rand.Shuffle(len(allModels), func(i, j int) { allModels[i], allModels[j] = allModels[j], allModels[i] })
+
+	if err := DB.Migrator().DropTable(allModels...); err != nil {
+		log.Printf("Failed to drop table, got error %v\n", err)
+		os.Exit(1)
+	}
+
+	if err := DB.AutoMigrate(allModels...); err != nil {
+		log.Printf("Failed to auto migrate, but got error %v\n", err)
+		os.Exit(1)
+	}
+
+	for _, m := range allModels {
+		if !DB.Migrator().HasTable(m) {
+			log.Printf("Failed to create table for %#v\n", m)
+			os.Exit(1)
+		}
 	}
 }
